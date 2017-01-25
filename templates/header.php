391dd984f538cf13defd667bacb8a3d5c2bd99ca
Adi Ardana <adhixz@gmail.com>
2017-01-26 00:23:35 +0800

    add bootstrap navwalker

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,36 @@
 <header class="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+
+    <nav class="navbar navbar-default nav-primary" role="navigation">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand visible-xs" href="<?php echo home_url(); ?>"><?php _e('Menu', 'sage');?></a>
+        </div>
+
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+              wp_nav_menu( array(
+                  'theme_location'    => 'primary_navigation',
+                  'depth'             => 2,
+                  'container'         => 'div',
+                  'container_class'   => 'collapse navbar-collapse',
+                  'container_id'      => 'bs-example-navbar-collapse-1',
+                  'menu_class'        => 'nav navbar-nav',
+                  'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                  'walker'            => new wp_bootstrap_navwalker())
+              );
+            endif;
+          ?>
+        </div>
     </nav>
+
   </div>
 </header>
