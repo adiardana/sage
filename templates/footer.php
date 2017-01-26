b88543a1f2b7ce01fc323f7b6583ecf4974c18be
Adi Ardana <adhixz@gmail.com>
2017-01-27 00:04:15 +0800

    add copyright text

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,14 @@
 <footer class="content-info">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+      <div class="col-sm-12">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <p>Copyright &copy;<?php echo date('Y');?> | <a href="<?php echo home_url();?>"><?php bloginfo('name'); ?></a></p>
+      </div>
+    </div>
   </div>
 </footer>
