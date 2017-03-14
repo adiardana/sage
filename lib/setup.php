c49c58257adf6ed8ca222212dcdfb77fc8172438
AU\adi.ardana <adi.ardana@ogilvy.com.au>
2017-03-14 08:08:12 +0800

    update js lib file

--- lib/setup.php
+++ lib/setup.php
@@ -100,7 +100,8 @@ function assets() {
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
-  wp_enqueue_script('sage/lib', Assets\asset_path('scripts/lib.js'), ['jquery'], null, true);
+  // wp_enqueue_script('sage/lib', Assets\asset_path('scripts/flickity.js'), ['jquery'], null, true);
+  // wp_enqueue_script('sage/lib', Assets\asset_path('scripts/fancybox.js'), ['jquery'], null, true);
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
