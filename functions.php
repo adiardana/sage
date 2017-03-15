a5a4490f5024bc95bb8b17fe9b9bd71b5e610b26
AU\adi.ardana <adi.ardana@ogilvy.com.au>
2017-03-15 12:48:29 +0800

    update functions.php

--- functions.php
+++ functions.php
@@ -15,7 +15,9 @@ $sage_includes = [
   'lib/setup.php',     // Theme setup
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
-  'lib/customizer.php' // Theme customizer
+  'lib/customizer.php',   // Theme customizer
+  'lib/navwalker.php',    // Menu navwalker
+  'lib/theme.php'         // Theme functions
 ];
 
 foreach ($sage_includes as $file) {
@@ -26,3 +28,9 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+function dump($var) {
+  echo '<pre>';
+  print_r($var);
+  echo '</pre>';
+}
