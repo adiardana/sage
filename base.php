35b5aea21e8a34ab9e7c016593f813cedb9d7334
Adi Ardana <adhixz@gmail.com>
2017-04-07 11:53:51 +0800

    remove after_body hook

--- base.php
+++ base.php
@@ -9,10 +9,6 @@ use Roots\Sage\Wrapper;
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
-    <?php
-    // add after body hooks, for google analytics or other tracking scripts
-    do_action('after_body');
-    ?>
     <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
